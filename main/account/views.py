1dcb963c7706407b6bd146a3a72c238c03dfd1c1
myatthu000 <september.2019thu@gmail.com>
2025-12-05 16:44:33 +0630

    login, register & dashboard

---
+++ main/account/views.py
@@ -0,0 +1,114 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.shortcuts import render
+from .forms import (
+    LoginForm, UserRegisteratinForm,
+    UserEditForm, ProfileEditForm
+)
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from .models import Profile
+
+
+# Create your views here.
+
+
+# def user_login(request):
+
+#     if request.user.is_authenticated:
+#         return redirect('dashboard')
+
+#     if request.method == 'POST':
+#         form = LoginForm(request.POST)
+#         if form.is_valid():
+#             cd = form.cleaned_data
+#             user = authenticate(
+#                 request,
+#                 username=cd['username'],
+#                 password=cd['password'],
+#             )
+#             if user is not None:
+#                 if user.is_active():
+#                     login(request, user)
+#                     # return HttpResponse('Authenticated successfully.')
+#                     return redirect('dashboard')
+#                 else:
+#                     return HttpResponse('Disabled account.')
+#             else:
+#                 return HttpResponse('Invalid Login.')
+#     else:
+#         form = LoginForm()
+#     return render(request, 'account/login.html',{
+#         'form': form
+#     })
+
+@login_required
+def dashboard(request):
+    return render(
+        request,
+        'account/dashboard.html',
+        {'section': 'dashboard'}
+    )
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(
+            instance=request.user,
+            data=request.POST
+        )
+        profile_form = ProfileEditForm(
+            instance=request.user.profile,
+            data=request.POST,
+            files=request.FILES
+        )
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(
+            instance=request.user
+        )
+        profile_form = ProfileEditForm(
+            instance=request.user.profile
+        )
+    return render(
+        request,
+        'account/edit.html',
+        {
+            'user_form': user_form,
+            'profile_form': profile_form
+        }
+    )
+
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('dashboard')
+    
+    if request.method == 'POST':
+        user_form = UserRegisteratinForm(request.POST)
+        if user_form.is_valid():
+            # create a new user obj but avoid saving it
+            new_user = user_form.save(commit=False)
+            # set the chosen password
+            new_user.set_password(
+                user_form.cleaned_data['password']
+            )
+            # save the user obj
+            new_user.save()
+            # create the user profile
+            Profile.objects.create(user=new_user)
+            return render(
+                request,
+                'account/register_done.html',
+                {
+                    'new_user': new_user
+                }
+            )
+    else:
+        user_form = UserRegisteratinForm()
+    return render(
+        request,
+        'account/register.html',
+        {'user_form': user_form}
+    )
